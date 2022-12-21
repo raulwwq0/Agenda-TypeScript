a6abef05eb6cf92529aea09e51e0f0e64988eed7
Raúl Aguilar <ag21raul@gmail.com>
2022-12-21 16:57:39 +0100

    chore(apps/server): folder structure

--- apps/server/app.ts
+++ apps/server/app.ts
@@ -6,10 +6,6 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('First Express + TypeScript Server');
-});
-
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
